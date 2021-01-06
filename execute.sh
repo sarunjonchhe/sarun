f0efad665cdffe67cbdf394753c02984dff74d98
sarunjonchhe <sarunjonchhe@gmail.com>
2021-01-06 08:44:04 +0545

    Add execute.sh

---
+++ execute.sh
@@ -0,0 +1,7 @@
+#execute it everytime to sync in github
+git add .
+git commit -m "Add execute.sh"
+git init
+git remote add origin https://github.com/sarunjonchhe/sarun.git
+git push -u origin master
+
